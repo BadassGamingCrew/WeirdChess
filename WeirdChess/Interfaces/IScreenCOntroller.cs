0d72f274d86f33340b2829cb201facf63d92f251
Miroslav Galabov <wolfnature13@gmail.com>
2015-11-26 23:10:46 +0200

    SRP modifications
    Added an IIterable interface.
    IScreenController implements IIterable, instead of IIterator.

--- WeirdChess/Interfaces/IScreenCOntroller.cs
+++ WeirdChess/Interfaces/IScreenCOntroller.cs
@@ -6,9 +6,9 @@ namespace WeirdChess.Interfaces
     /// <summary>
     /// Interface for Screen Controller objects.
     /// Provides methods for managing the IGameScreen collection.
-    /// Also provides iteration methods for navigating between the different IGameScreen objects.
+    /// Also provides an iterator for navigating between the different IGameScreen objects.
     /// </summary>
-    public interface IScreenController : IIterator<IGameScreen>
+    public interface IScreenController : IIterable<IGameScreen>
     {
         /// <summary>
         /// Changes the value for the GameScreenType argument, to the IGameScreen argument.
